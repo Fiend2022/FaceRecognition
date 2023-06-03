b2576c08e90d2af93044e59231a972c3bc471345
Fiend2022 <chili_i@mail.ru>
2023-06-03 19:52:08 +0500

    add eval

---
+++ evaluation.py
@@ -0,0 +1,64 @@
+def getDataOneWithMatch(filename:str):
+    with open(filename, 'r') as file:
+        text = file.readlines()
+        count = len(text)
+        lines = [l.rstrip() for l in text]
+        data = []
+        for i in lines:
+            splitString = i.split(' ')
+            template = splitString[1]
+            condidate = splitString[0]
+            label = int(splitString[2])
+            data.append((template, condidate, label))
+    return data, count
+
+def checkLabelCorrect(pair:tuple[str, str, int]):
+    class1 = int((pair[0].split('/', 1)[0]))
+    class2 = int((pair[1].split('/', 1)[0]))
+    if class1 == class2 and pair[2] == 1:
+        return True
+    elif class1 == class2 and pair[2] == 0:
+        return False
+    elif class1 != class2 and pair[2] == 1:
+        return False
+    elif class1 != class2 and pair[2] == 0:
+        return True
+
+def getDataBestComp(filename:str):
+    with open(filename, 'r') as file:
+        text = file.readlines()
+        count = len(text)
+        lines = [l.rstrip() for l in text]
+        data = []
+        for i in lines:
+            splitString = i.split(' ')
+            template = splitString[1]
+            condidate = splitString[0]
+            data.append((template, condidate))
+    return data, count
+
+def checkPairCorrect(pair:tuple[str, str]):
+    class1 = int((pair[0].split('/', 1)[0]))
+    class2 = int((pair[1].split('/', 1)[0]))
+    if class1 == class2:
+        return True
+    else:
+        return False
+
+outputs, numOfTest = getDataOneWithMatch("VGG16OneWithAll.txt")
+errors = 0
+for i in outputs:
+    res = checkLabelCorrect(i)
+    if res == False:
+        errors += 1
+
+print("Number of errors: ", errors, "out of", numOfTest)
+
+outputs, numOfTest = getDataBestComp("VGG16BestComplience.txt")
+errors = 0
+for i in outputs:
+    res = checkPairCorrect(i)
+    if res == False:
+        errors += 1
+
+print("Number of errors: ", errors, "out of", numOfTest)
---
+++ predict.py
@@ -0,0 +1,103 @@
+# face verification with the VGGFace2 model
+from matplotlib import pyplot
+from PIL import Image
+from numpy import asarray
+from scipy.spatial.distance import *
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
+from sklearn.metrics import f1_score
+
+# extract a single face from a given photograph
+def extract_face(filename, required_size=(224, 224)):
+    # load image from file
+    pixels = Image.open('animal/' + filename)
+    face = pixels.resize(required_size)
+    face_array = asarray(face)
+    return face_array
+
+
+
+# extract faces and calculate face embeddings for a list of photo files
+def get_embeddings(filenames):
+    # extract faces
+    faces = [extract_face(f) for f in filenames]
+    # convert into an array of samples
+    samples = asarray(faces, 'float32')
+    # prepare the face for the model, e.g. center pixels
+    samples = preprocess_input(samples, version=2)
+    # create a vggface model
+    model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+    # perform prediction
+    yhat = model.predict(samples)
+    return yhat
+
+
+# determine if a candidate face is a match for a known face
+def is_match(known_embedding, candidate_embedding, thresh=0.10):
+    # calculate distance between embeddings
+    score = cosine(known_embedding[0], candidate_embedding[0])
+    if score > thresh:
+        return 0, score
+    else:
+        return 1, score
+
+
+# define filenames
+templateFilenames = []
+exampleFilenames  = []
+
+with open('template.txt', 'r') as file1:
+    lines = file1.readlines()
+    lines = [l.rstrip() for l in lines]
+    for line in lines:
+        #line = 'animal/' + line
+        templateFilenames.append(line)
+with open('example.txt', 'r') as file2:
+    lines = file2.readlines()
+    lines = [l.rstrip() for l in lines]
+    for line in lines:
+        #line = 'animal/' + line
+        exampleFilenames.append(line)
+
+# get embeddings file filenames
+templateEmbeddings = get_embeddings(templateFilenames)
+exampleEmbeddings = get_embeddings(exampleFilenames)
+
+templatesData = []
+
+for temp, file in zip(templateEmbeddings, templateFilenames):
+    templatesData.append((temp, file))
+
+examplesData = []
+for example, file in zip(exampleEmbeddings, exampleFilenames):
+        examplesData.append((example, file))
+
+results = []
+
+with open("Resnet50OneWithAll.txt", "w") as resultFile:
+    for face in examplesData:
+        metaData = []
+        for template in templatesData:
+            label, score = is_match(template, face)
+            resultFile.write(template[1] + " " + face[1] + " " + str(label) + "\n")
+            md = (template[1], score, label)
+            metaData.append(md)
+        res = (face[1], metaData)
+        results.append(res)
+
+with open("Resnet50BestComplience.txt", "w") as resultFile:
+    for example in results:
+        data = example[1]
+        complience = 1
+        bestTemplate = ""
+        for info in data:
+            currentComp = info[1]
+            currentTemplate = info[0]
+            if (currentComp < complience):
+                complience = currentComp
+                bestTemplate = currentTemplate
+        resultFile.write(example[0] + " " + bestTemplate + "\n")
+
+
+
+
